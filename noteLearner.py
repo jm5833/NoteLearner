3fdce79003862a03186ce9c9b7b0dd318ee096f3
Jack Martinez <jm5833@nyu.edu>
2020-06-07 18:24:15 -0400

    Added User class definition to be used in sqlite

--- noteLearner.py
+++ noteLearner.py
@@ -1,8 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'bac33467956c034b85718b84bbde214b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+#Class model for the user table in sqlite
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return self.username
+
+#site routes 
 @app.route('/')
 def home():
     return 'hello world'
